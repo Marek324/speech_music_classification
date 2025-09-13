529f19cd89b091f0adffe215bb2210ebac33a4ec
Marek324 <mhric24@gmail.com>
2025-09-13 21:07:11 +0200

    wip: features

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import sys
 
 if "-h" in sys.argv or "--help" in sys.argv:
     # TODO: implement help message
-    print("Usage: python main.py")
+    print(f"Usage: python {sys.argv[0]}")
     sys.exit(0)
 
 try:
@@ -28,6 +28,7 @@ def main():
     train()
     evaluate()
 
+
 # dir = {speech, music}
 def train(dir:str):
     prefix = DATASET_PATH + f"/train/{dir}"
@@ -35,6 +36,51 @@ def train(dir:str):
         file_path = f"{prefix}/{file}"
         s = load_and_resample(file_path, SAMPLE_RATE)
         features = create_features(s)
+
+
+def short_time_energy(frame: np.ndarray) -> float:
+    return 10 * np.log10(1/frame.shape[0] * np.sum(frame**2))
+
+
+def zero_crossing_rate(frame: np.ndarray) -> float:
+    count = 0
+    for i in range(1, frame.shape[0]):
+        if frame[i] * frame[i - 1] < 0:
+            count += 1
+    return count / 2
+
+
+# 0-70, 11k-44k1
+def band_energy_ratio(frame: np.ndarray, fl_1: float, fh_1: float, fl_2: float, fh_2: float) -> float:
+    def bin_num(f: float, Fs: int, K: int) -> int:
+        return int(np.floor((K * f) / Fs))
+
+    
+    def band_energy(dft: np.ndarray, fl: float, fh: float) -> float:
+        global SAMPLE_RATE
+        K = dft.shape[0]
+        b1 = bin_num(fl, SAMPLE_RATE, K)
+        b2 = bin_num(fh, SAMPLE_RATE, K)
+        return np.sum(np.abs(dft[b1:b2])**2)
+
+
+    dft = np.fft.fft(frame)
+    E1 = band_energy(dft, fl_1, fh_1)
+    E2 = band_energy(dft, fl_2, fh_2)
+    return 10 * np.log10(E1 / E2) if E2 > 0 else np.inf
+
+
+def autocorrelation_coef(frame: np.ndarray)-> float:
+    R = np.correlate(frame, frame, mode='full')
+    R = R[R.shape[0] // 2:] 
+    m1 = np.floor(3 * SAMPLE_RATE / 1000).astype(int)  # 3ms
+    m2 = np.floor(16 * SAMPLE_RATE / 1000).astype(int) # 16ms
+    return np.max(R[m1:m2]) 
+
+
+def mfcc(frame: np.ndarray)-> np.ndarray:
+    coeffs = lb.feature.mfcc(y=frame, sr=SAMPLE_RATE, n_mfcc=10)
+
 
 def create_features(s: np.ndarray) -> np.ndarray:
     features = np.array([])
@@ -44,11 +90,13 @@ def create_features(s: np.ndarray) -> np.ndarray:
 
     return features
 
+
 def is_silence(frame: np.ndarray, threshold: int = -20) -> bool:
     # Normalized short time energy
     E = np.sum(np.abs(np.fft.rfft(a=frame, n=len(frame)))**2) / len(frame)**2
     db = 10 * np.log10(E) if E > 0 else -np.inf
     return db < threshold
+
 
 def framing(s: np.ndarray, fl_ms:int = FRAME_LEN_MS, fh_ms:int = FRAME_HOP_MS) -> np.ndarray:
     fl = int(fl_ms * SAMPLE_RATE / 1000)  # frame length [samples]
@@ -60,12 +108,15 @@ def framing(s: np.ndarray, fl_ms:int = FRAME_LEN_MS, fh_ms:int = FRAME_HOP_MS) -
 
     return np.array([s[i * fs:i * fs + fl] * hann_win for i in range(Nf)])
 
+
 def evaluate():
     ...
+
 
 def load_and_resample(file_path:str, target_fs:int) -> np.ndarray:
     s = lb.load(file_path, sr=target_fs)[0] # mono only for now
     return s
+
 
 if __name__ == "__main__":
     main()
