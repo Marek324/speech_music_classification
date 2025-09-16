cf04c53e9af41fdf4062980b9c93c35666576164
Marek324 <mhric24@gmail.com>
2025-09-17 01:11:33 +0200

    add default ref path

--- defaults.py
+++ defaults.py
@@ -6,3 +6,4 @@ FRAME_LEN_MS = 40  # Frame length in milliseconds
 FRAME_HOP_MS = 20  # Frame hop in milliseconds
 SEGMENT_LEN_S = 3  # Segment length in seconds
 DATASET_PATH = "dataset/scheirer-slaney/wavfile"  # Path to the dataset
+REF_PATH = "dataset/scheirer-slaney/reference.csv"  # Path to the reference file
