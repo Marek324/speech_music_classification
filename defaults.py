0ec191c5058ca360a83b199ecb8b7f3265e9aaa7
Marek324 <mhric24@gmail.com>
2025-09-20 18:51:17 +0200

    udpate defaults

--- defaults.py
+++ defaults.py
@@ -5,5 +5,5 @@ SAMPLE_RATE = 44100  # Default sample rate
 FRAME_LEN_MS = 40  # Frame length in milliseconds
 FRAME_HOP_MS = 20  # Frame hop in milliseconds
 SEGMENT_LEN_S = 3  # Segment length in seconds
-DATASET_PATH = "dataset/scheirer-slaney/wavfile"  # Path to the dataset
-REF_PATH = "dataset/scheirer-slaney/reference.csv"  # Path to the reference file
+DATASET_PATH = "dataset"  # Path to the dataset
+REF_PATH = "dataset/reference.csv"  # Path to the reference file
