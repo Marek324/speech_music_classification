70f6f70bcb1d375b9e223919e3a0284dc26b1c30
Marek324 <mhric24@gmail.com>
2025-12-21 20:09:44 +0100

    job fix

--- job.sh
+++ job.sh
@@ -7,7 +7,7 @@ module load ffmpeg
 
 source .env
 
-uv run huggingface-cli download Marek324/speech-music-classification --repo-type dataset --token $HF_TOKEN > out_hf_cli_download.log 2> err_hf_cli_download.log
+uvx hf download Marek324/speech-music-classification --repo-type dataset --token $HF_TOKEN > out_hf_cli_download.log 2> err_hf_cli_download.log
 
 #uv run src/main.py train > out_train.log 2> err_train.log
 uv run src/main.py eval > out_eval.log 2> err_eval.log
