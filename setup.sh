f4341358c050d287a5946e13355e107a80867426
Marek324 <mhric24@gmail.com>
2026-03-14 23:59:40 +0000

    m

--- setup.sh
+++ setup.sh
@@ -5,6 +5,7 @@ git lfs install
 git config pull.rebase true
 git config pull.ff only
 git config user.name "Marek324"
+git lfs pull
 curl -fsSL https://claude.ai/install.sh | bash
 curl -fsSL https://claude.ai/install.sh | bash
 echo "Don\'t forget to add HF token, git user.email, wandb API key, and run source ~/.bashrc"
